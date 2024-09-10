a500d52ca1f244cfa1c2abf5a5d6d9d6c528635f
Son421 <115635389+Son421@users.noreply.github.com>
2024-09-10 11:55:24 +0300

    fix total price bug

--- src/features/cartSlice.tsx
+++ src/features/cartSlice.tsx
@@ -10,6 +10,15 @@ const loadCartFromLocalStorage = (): cartProducts[] => {
     }
 };
 
+const loadTotalPriceFromLocalStorage = (): number => {
+  const totalPrice = localStorage.getItem('totalPrice');
+  if (totalPrice === null) {
+    return 0;
+  }else{
+    return JSON.parse(totalPrice);
+  }
+};
+
 interface cartProducts {
   _id: number;
   title: string;
@@ -28,12 +37,14 @@ interface cartProductState {
 const initialState: cartProductState = {
     value: loadCartFromLocalStorage(),   
     isPopupOpen: false,
-    totalPrice: 0,
+    totalPrice: loadTotalPriceFromLocalStorage(),
 }
 
-const saveCartToLocalStorage = (cart: cartProducts[]) => {
+const saveCartToLocalStorage = (cart: cartProducts[], totalPrice: number) => {
     const savedCart = JSON.stringify(cart);
+    const savedTotalPrice = JSON.stringify(totalPrice);
     localStorage.setItem('cart', savedCart);
+    localStorage.setItem('totalPrice', savedTotalPrice);
 };
 
 export const cartProductSlice = createSlice({
@@ -51,36 +62,39 @@ export const cartProductSlice = createSlice({
           const index = state.value.findIndex(product => product._id === action.payload._id);
           state.value[index].quantity = state.value[index].quantity + 1;
         }
-        saveCartToLocalStorage(state.value);
+        saveCartToLocalStorage(state.value, state.totalPrice);
       },
       deleteProduct: (state, action: PayloadAction<cartProducts>) => {
         state.value = state.value.filter( item => item._id !== action.payload._id);
-        saveCartToLocalStorage(state.value);
+        saveCartToLocalStorage(state.value, state.totalPrice);
       },
       clearCart: (state) => {
         state.value = [];
-        saveCartToLocalStorage(state.value);
+        saveCartToLocalStorage(state.value, state.totalPrice);
       },
       plusProduct: (state, action: PayloadAction<cartProducts>) => {
         const index = state.value.findIndex(product => product._id === action.payload._id);
         state.value[index].quantity = state.value[index].quantity + 1;
-        saveCartToLocalStorage(state.value);
+        saveCartToLocalStorage(state.value, state.totalPrice);
       },
       minusProduct: (state, action: PayloadAction<cartProducts>) => {
         const index = state.value.findIndex(product => product._id === action.payload._id);
         state.value[index].quantity = state.value[index].quantity - 1;
-        saveCartToLocalStorage(state.value);
+        saveCartToLocalStorage(state.value, state.totalPrice);
       },
       setTotalPrice: (state,  action: PayloadAction<string>) =>{
         switch (action.payload) {
           case 'EUR':
             state.totalPrice = +state.value.reduce((sum, product) => sum + product.price * 0.90 * product.quantity, 0).toFixed(2);
+            saveCartToLocalStorage(state.value, state.totalPrice);
             break;
           case 'UAH':
             state.totalPrice = +state.value.reduce((sum, product) => sum + product.price * 41 * product.quantity, 0).toFixed(2);
+            saveCartToLocalStorage(state.value, state.totalPrice);
             break;
           default:
             state.totalPrice = +state.value.reduce((sum, product) => sum + product.price * product.quantity, 0).toFixed(2);
+            saveCartToLocalStorage(state.value, state.totalPrice);
         }
       }
     },
